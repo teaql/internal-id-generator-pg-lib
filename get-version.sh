de6f041b17f0e86c047b939e99d24cbca34a99f9
Philip Zhang <zhangxilai@doublechaintech.com>
2023-05-16 17:54:58 +0800

    update to version:

--- get-version.sh
+++
@@ -1,4 +0,0 @@
-
-version=$(xmlstarlet select -t -v '/root/@version' -n models/main.xml)
-new_version=$((version + 1))
-echo "$new_version"
--- increase-version.sh
+++ increase-version.sh
@@ -1,4 +1,6 @@
-new_version=$(bash get-version.sh)
+version=$(xmlstarlet select -t -v '/root/@version' -n models/main.xml)
+new_version=$((version + 1))
+
 xmlstarlet ed -u '/root/@version' -v ${new_version} models/main.xml > temp.xml
 cat temp.xml > models/main.xml
 rm temp.xml
